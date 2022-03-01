a076f1404e75ac3d054818a7a52a9eb095515265
tilenn <tilen.ozbot@gmail.com>
2022-03-01 20:32:24 +0100

    1.dn

--- 1.naloga/naloga1.py
+++ 1.naloga/naloga1.py
@@ -1,5 +1,10 @@
+import collections
+from itertools import count
+from math import log2
+
+
 def naloga1(besedilo, p):
-    """ Izracun povprecne nedolocenosti na znak 
+    """Izracun povprecne nedolocenosti na znak
 
     Parameters
     ----------
@@ -8,19 +13,59 @@ def naloga1(besedilo, p):
     p : int
         Stevilo poznanih predhodnih znakov: 0, 1 ali 2.
         p = 0: H(X1)
-            racunamo povprecno informacijo na znak abecede 
+            racunamo povprecno informacijo na znak abecede
             brez poznanih predhodnih znakov
         p = 1: H(X2|X1)
-            racunamo povprecno informacijo na znak abecede 
-            pri enem poznanem predhodnemu znaku 
+            racunamo povprecno informacijo na znak abecede
+            pri enem poznanem predhodnemu znaku
         p = 2: H(X3|X1,X2)
 
     Returns
     -------
-    H : float 
-        Povprecna informacija na znak abecede z upostevanjem 
+    H : float
+        Povprecna informacija na znak abecede z upostevanjem
         stevila poznanih predhodnih znakov 'p'
     """
 
-    H = None
-    return H
+    # formatira besedilo v bolj prijazno obliko
+    besedilo = "".join([crka for crka in besedilo if crka.isalpha()]).upper()
+
+    counter_za_crke = collections.Counter(besedilo)
+    # print(counter_za_crke)
+
+    str_len = len(besedilo)
+    H = 0
+
+    for letter, val in counter_za_crke.items():
+        H -= (val / str_len) * log2(val / str_len)
+
+    # print(H)
+    if p == 0:
+        return H
+
+    dvojcki = collections.defaultdict(int)
+    for i, j in zip(besedilo, besedilo[1:]):
+        dvojcki[i + j] += 1
+
+    st_vseh_dvojckov = sum(dvojcki.values())
+    H2 = 0
+    for val in dvojcki.values():
+        H2 += (val / st_vseh_dvojckov) * log2(st_vseh_dvojckov / val)
+    print(H2)
+
+    if p == 1:
+        return H2 - H
+
+    trojcki = collections.defaultdict(int)
+    for i, j, k in zip(besedilo, besedilo[1:], besedilo[2:]):
+        trojcki[i + j + k] += 1
+
+    st_vseh_trojckov = sum(trojcki.values())
+
+    H3 = 0
+    for val in trojcki.values():
+        H3 += (val / st_vseh_trojckov) * log2(st_vseh_trojckov / val)
+    print(H3)
+    return H3 - H2
+
+    # return H
--- 1.naloga/test_naloga1.py
+++ 1.naloga/test_naloga1.py
@@ -10,15 +10,31 @@ def test_naloga1(case_dir, case_id):
     Primer zagona:
     from test_naloga1 import test_naloga1
     test_naloga1('primeri',1)
-    """    
+    """
     base_path = Path(__file__).parent
-    file_path = (base_path / str(case_dir) / (str(case_id) + '.json')).resolve()
-    json_file = open(file_path, 'r', encoding='utf8')
+    file_path = (base_path / str(case_dir) / (str(case_id) + ".json")).resolve()
+    json_file = open(file_path, "r", encoding="utf8")
     tol = 1e-3
     data = json.load(json_file, strict=False)
     start = timer()
-    H = naloga1(data['besedilo'], data['p'])
+    H = naloga1(data["besedilo"], data["p"])
     end = timer()
-    success = (abs(H-data['H']) < tol)
-    print('Rezultat za primer', case_id, ':', success, ';', 'Izracunani H:', H, ';', 'Pravi H:', float(data['H']))
-    print(f'Cas izvajanja: {end-start:.2f} s')
+    success = abs(H - data["H"]) < tol
+    print(
+        "Rezultat za primer",
+        case_id,
+        ":",
+        success,
+        ";",
+        "Izracunani H:",
+        H,
+        ";",
+        "Pravi H:",
+        float(data["H"]),
+    )
+    print(f"Cas izvajanja: {end-start:.2f} s")
+
+
+test_naloga1("./primeri", 1)
+test_naloga1("./primeri", 2)
+test_naloga1("./primeri", 3)
