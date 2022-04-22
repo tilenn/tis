fb9b3fb2c2b8adbfe303b141fa6b0399ce011a1d
tilenn <to8803@student.uni-lj.si>
2022-04-23 00:58:03 +0200

    H matrika narejena i guess

--- 3.naloga/naloga3.py
+++ 3.naloga/naloga3.py
@@ -1,16 +1,20 @@
+import numpy as np
+from math import log2
+
+
 def naloga3(vhod: list, n: int) -> tuple[list, str]:
     """
-    Izvedemo dekodiranje binarnega niza `vhod`, zakodiranega 
+    Izvedemo dekodiranje binarnega niza `vhod`, zakodiranega
     s Hammingovim kodom dolzine `n` in poslanega po zasumljenem kanalu.
     Nad `vhod` izracunamo vrednost `crc` po standardu CRC-8/CDMA2000.
 
     Parameters
     ----------
     vhod : list
-        Sporocilo y, predstavljeno kot seznam bitov (stevil tipa int) 
+        Sporocilo y, predstavljeno kot seznam bitov (stevil tipa int)
     n : int
         Stevilo bitov v kodni zamenjavi
-    
+
     Returns
     -------
     (izhod, crc) : tuple[list, str]
@@ -19,6 +23,33 @@ def naloga3(vhod: list, n: int) -> tuple[list, str]:
         crc : str
             Vrednost CRC, izracunana nad `vhod`. Niz dveh znakov.
     """
+
+    # vhod
+    y = np.array(vhod, dtype=np.uint8).reshape(len(vhod) // n, n)
+    print("y (vhodno sporocilo): ", y, sep="\n")
+
+    # stevilo varnostnih bitov
+    m = int(log2(n + 1))
+
+    # matrika H (oziroma H transponirano), dimenzije n x m
+    # po vrsticah ima binarno zapisana stevila
+    # v zadnjih vrsticah so potence stevila 2, tako da rata identiteta
+
+    tmp = np.unpackbits(
+        np.array(
+            [i for i in range(1, n + 1) if (i & (i - 1) != 0)],
+            dtype=np.uint8,
+        ),
+        bitorder="little",
+    ).reshape(n - m, 8)[:, :m]
+    # print(tmp)
+
+    m_identity_matrix = np.eye(m, dtype=np.uint8)
+    # print(m_identity_matrix)
+
+    H_transponse = np.vstack((tmp, m_identity_matrix))
+    print(H_transponse)
+
     izhod = []
-    crc = ''
+    crc = ""
     return (izhod, crc)
